d2fd2dc0bc45ea4a03f48761c88b0b82d10e476b
phrynus <fondantcn@gmail.com>
2025-07-10 15:26:11 +0800

    添加了 `vite-plugin-compression` 插件以支持文件压缩，更新了相关配置；删除了 `ZCOOLKuaiLe-Regular.ttf` 字体文件；调整了 `main.scss` 中的字体定义格式。

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import VueDevTools from "vite-plugin-vue-devtools";
-
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
+import viteCompression from 'vite-plugin-compression';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,23 +14,31 @@ export default defineConfig({
     vue(),
     VueDevTools(),
     AutoImport({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
-    })
+      resolvers: [ElementPlusResolver()],
+    }),
+    viteCompression({
+      verbose: true, // 是否在控制台中输出压缩结果
+      disable: false,
+      threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+      algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+      ext: '.gz',
+      deleteOriginFile: true, // 源文件压缩后是否删除(我为了看压缩后的效果，先选择了true)
+    }),
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "~": fileURLToPath(new URL("./src", import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ["legacy-js-api"]
-      }
-    }
-  }
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
 });

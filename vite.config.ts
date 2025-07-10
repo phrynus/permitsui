d3657f402ec503c629781d931892a5d82d8d2789
phrynus <fondantcn@gmail.com>
2025-07-10 15:33:26 +0800

    更新 `vite.config.ts` 文件中的压缩配置，将 `deleteOriginFile` 选项从 `true` 修改为 `false`，以保留源文件。

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
       threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
       algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
       ext: '.gz',
-      deleteOriginFile: true, // 源文件压缩后是否删除(我为了看压缩后的效果，先选择了true)
+      deleteOriginFile: false, // 源文件压缩后是否删除(我为了看压缩后的效果，先选择了true)
     }),
   ],
   resolve: {

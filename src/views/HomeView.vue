8e3dd4236b22fec1122e67984352f6d8723f1433
phrynus <fondantcn@gmail.com>
2025-07-28 17:12:27 +0800

    在许可证类型中添加“出版物”，并修正“多方通讯”为“多方通信”

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,7 +36,7 @@ const screenBox: any = ref(false);
 const { proxy } = getCurrentInstance()!;
 
 // 可选的许可证类型标签
-const tags: any = ref(['ICP', 'EDI', 'ISP', 'IDC', 'CDN', 'VPN', 'SP', '多方通讯', '呼叫中心业务', '文网文', '变更']);
+const tags: any = ref(['ICP', 'EDI', 'ISP', 'IDC', 'CDN', 'VPN', 'SP', '多方通信', '出版物', '呼叫中心业务', '文网文', '变更']);
 
 // 可选地区列表
 const areas = ref([
